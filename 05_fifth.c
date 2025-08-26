7a7ad402f2cd228b630fe1535fd549941de379df
Ayush Srivastava <srivastavayush040@gmail.com>
2025-08-26 17:11:40 +0530

    escape sequence

---
+++ 05_fifth.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    a=10;
+    printf("i am %d times more handsome than you \n i am just kidding dont't mind", a);
+    //here i leanrned about escape sequence
+    return 0;
+}
