0f35ad6eb8be3a274fd720ed7d9516dcf2a80341
Ayush Srivastava <srivastavayush040@gmail.com>
2025-08-26 09:51:11 +0530

    Added second program for basic maths operations

---
+++ 02_second.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a = 20, b = 5;
+
+    printf("a = %d, b = %d\n", a, b);
+
+    printf("Addition: %d + %d = %d\n", a, b, a + b);
+    printf("Subtraction: %d - %d = %d\n", a, b, a - b);
+    printf("Multiplication: %d * %d = %d\n", a, b, a * b);
+    printf("Division: %d / %d = %d\n", a, b, a / b);
+
+    return 0;
+}
