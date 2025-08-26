7dbc93c71dee02d7424d112c31366e49cf27f150
Ayush Srivastava <srivastavayush040@gmail.com>
2025-08-26 16:14:55 +0530

    float and char

---
+++ 03_third.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int a=3;
+    float b=1.3;
+    char c='a';
+    //so this my first code with float and char (smile)
+    printf("the result is %d",a+b*c);
+    }
