b7dd82317c8e65a189e7d474beabd706e202f287
Ayush Srivastava <srivastavayush040@gmail.com>
2025-08-26 17:47:29 +0530

    some minor changes

---
+++ 01_third.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/* project-- 03_third.c
+ author- ayush srivastava
+ date- 28.08.2025*/
+int main(){
+    int a=3;
+    int b=1;
+    char c='A';
+    //so this my first code with float and char (smile)
+    printf("the result is %d\n",a+b);
+    printf("the character c is %c\n",c);
+    printf("The ASCII value of c is %d\n", c);
+    printf("The sum of a, b, and c (using ASCII) is %d\n", a + b + c);
+    return 0;
+    }
