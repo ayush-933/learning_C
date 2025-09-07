596b1cbee75a9523a184df10045dbd6d359067de
Ayush Srivastava <srivastavayush040@gmail.com>
2025-09-07 15:47:52 +0530

    loop

---
+++ 13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("enter the first number:");
+    scanf("%d",&a);
+    for(int b=1;b<=10;b++){
+        printf("%d\n",a*b);
+    }
+return 0;
+}
+
