79c81f50fdddbe76e1fb383e20a4bc7627c2e377
Ayush Srivastava <srivastavayush040@gmail.com>
2025-10-20 12:37:36 +0530

    array

---
+++ 45.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+
+ int arr[3][4][5];
+ for(int i=0;i<3;i++){
+    for(int j=0;j<4;j++){
+        for(int k=0;k<5;k++){
+            printf("%u ",&arr[i][j][k]);
+        }
+    }
+ }
+return 0;
+}
